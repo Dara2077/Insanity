f5629538ed1f451999a695e6a8b73a4c92892ce8
dara2077 <dbenyacar1@gmail.com>
2023-02-06 09:28:42 -0500

    Work from class

--- src/Board.java
+++ src/Board.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Board {
+public class Board {
+    private static String board;
+    public Board(){
+        board = "RRRR--BBBB";
+        System.out.println(board);
+    }
+
+    public static void setBoard(String newBoard){
+        board = newBoard;
+    }
+
+    public static String getBoard(){
+        return board;
+    }
 }
--- src/Move.java
+++ src/Move.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Move {
+public class Move {
+    public Move (int position, int numSpaces){
+
+
+    }
+
+    private boolean isRed(int position){
+        if (Board.getBoard().substring(position, position + 1).equals("R")){
+            return true;
+        }
+        return false;
+    }
+
+    public int newPosition(int position, int numSpaces){
+        int position2 = 0;
+        if (isRed(position)){
+            position2 = position + numSpaces;
+        }
+        else{
+            position2 = position - numSpaces;
+        }
+        return position2;
+    }
+
+    public int newBoard()
 }
